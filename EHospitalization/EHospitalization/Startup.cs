4848bbdae2f21b4cc116fd1762ca9f20471c656c
Sadeed Ullah Khan <sadeedullahkhan@gmail.com>
2024-05-11 03:57:29 +0500

    Update
    Initial Structure

---
+++ EHospitalization/EHospitalization/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EHospitalization.Startup))]
+namespace EHospitalization
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
