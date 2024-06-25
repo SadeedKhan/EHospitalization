0dea2830602e148368185095747f7fe3e6062cd0
DESKTOP-JGDC159\k <mamuna@mypaperlessoffice.com>
2024-06-25 12:57:08 +0500

    Updated code for
    login to index page with
    register

--- EHospitalization/EHospitalization/Areas/Identity/Controllers/AccountController.cs
+++ EHospitalization/EHospitalization/Areas/Identity/Controllers/AccountController.cs
@@ -159,7 +159,7 @@ namespace EHospitalization.Areas.Identity.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Home",new {area="Admin"});
                 }
                 AddErrors(result);
             }
